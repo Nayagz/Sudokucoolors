dc5b7ebb9c349ab027218c7cd29d8e2f6d9707f3
Nayagz <121751288+Nayagz@users.noreply.github.com>
2023-07-15 12:19:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let timer;
+let sec = 0;
+let min = 0;
+
 const colorGrid = [
     [1, 0, 0, 0],
     [0, 2, 0, 0],
@@ -16,7 +20,7 @@ window.onload = function() {
             const select = document.createElement("select");
             select.id = `cell-${i}-${j}`;
 
-            let colors = ["", "red", "blue", "green", "yellow"]; // Assign colors directly
+            let colors = ["", "red", "blue", "green", "yellow"];
             colors.forEach(color => {
                 const option = new Option(color, color);
                 select.options.add(option);
@@ -26,7 +30,7 @@ window.onload = function() {
             select.disabled = !!colorGrid[i][j];
 
             select.addEventListener('change', function() {
-                cell.style.backgroundColor = this.value; // Change cell color on select change
+                cell.style.backgroundColor = this.value;
             });
 
             cell.appendChild(select);
@@ -35,9 +39,30 @@ window.onload = function() {
 
         table.appendChild(row);
     }
+
+    timer = setInterval(function() {
+        sec++;
+        if (sec === 60) {
+            sec = 0;
+            min++;
+        }
+        document.getElementById('timer').innerText = `${min < 10 ? '0' + min : min}:${sec < 10 ? '0' + sec : sec}`;
+    }, 1000);
 };
 
 function checkSolution() {
+    clearInterval(timer);
+    let topScores = JSON.parse(localStorage.getItem('topScores')) || [];
+
+    topScores.push(min * 60 + sec);
+    topScores.sort((a, b) => a - b);
+
+    if (topScores.length > 5) {
+        topScores.length = 5; // Only keep top 5
+    }
+
+    localStorage.setItem('topScores', JSON.stringify(topScores));
+
     for (let i = 0; i < 4; i++) {
         const rowColors = new Set();
         const colColors = new Set();
@@ -46,8 +71,13 @@ function checkSolution() {
             const rowColor = document.getElementById(`cell-${i}-${j}`).value;
             const colColor = document.getElementById(`cell-${j}-${i}`).value;
 
-            if (rowColors.has(rowColor) || colColors.has(colColor)) {
-                document.getElementById("result").textContent = "Incorrect solution!";
+            if (rowColors.has(rowColor)) {
+                document.getElementById("result").textContent = "Same color detected in the row!";
+                return;
+            }
+
+            if (colColors.has(colColor)) {
+                document.getElementById("result").textContent = "Same color detected in the column!";
                 return;
             }
 
@@ -55,6 +85,11 @@ function checkSolution() {
             colColors.add(colColor);
         }
     }
-
+    
     document.getElementById("result").textContent = "Correct solution!";
+}
+
+function viewTopScores() {
+    let topScores = JSON.parse(localStorage.getItem('topScores')) || [];
+    alert('Top Scores (in seconds):\n' + topScores.join('\n'));
 }
